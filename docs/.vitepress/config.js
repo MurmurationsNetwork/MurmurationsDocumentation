41839defd616eb56c1c251ff27e09c5e04752a14
Geoff Turk <32190662+geoffturk@users.noreply.github.com>
2024-09-14 08:33:07 +0200

    Add stats counter (#47)

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,74 +1,87 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
 
 export default defineConfig({
   title: "Murmurations Docs",
   description: "Documentation for the Murmurations Protocol",
   lastUpdated: true,
+  head: [
+    [
+      "script",
+      {
+        src: "//stats.murmurations.network/count.js",
+        async: true,
+        "data-goatcounter": "https://docs.murmurations.network/count",
+      },
+    ],
+  ],
   themeConfig: {
-    
     editLink: {
-      pattern: 'https://github.com/MurmurationsNetwork/MurmurationsDocumentation/edit/main/docs/:path',
-      text: 'Edit this page on GitHub'
+      pattern:
+        "https://github.com/MurmurationsNetwork/MurmurationsDocumentation/edit/main/docs/:path",
+      text: "Edit this page on GitHub",
     },
 
-    logo: '/murmurations-logo.png',
+    logo: "/murmurations-logo.png",
 
     nav: [
-      { text: 'FAQs', link: '/faqs/general' },
-      { text: 'Developers', link: '/developers/environments' },
-      { text: 'Guides', link: '/guides/create-a-profile' }
+      { text: "FAQs", link: "/faqs/general" },
+      { text: "Developers", link: "/developers/environments" },
+      { text: "Guides", link: "/guides/create-a-profile" },
     ],
 
     sidebar: [
       {
-        text: 'About',
+        text: "About",
         items: [
-          {link: '/about/introduction', text: 'Introduction'},
-          {link: '/about/use-cases', text: 'Use Cases'},
-          {link: '/about/ontology-composability', text: 'Composable Ontologies'},
-          {link: '/about/common-terms', text: 'Common Terms'},
-          {link: '/about/architecture', text: 'Architecture'},
-          {link: '/about/roadmap', text: 'Roadmap'},
-          {link: '/about/contributing', text: 'Contributing'},
-          {link: '/about/getting-support', text: 'Getting Support'}
+          { link: "/about/introduction", text: "Introduction" },
+          { link: "/about/use-cases", text: "Use Cases" },
+          {
+            link: "/about/ontology-composability",
+            text: "Composable Ontologies",
+          },
+          { link: "/about/common-terms", text: "Common Terms" },
+          { link: "/about/architecture", text: "Architecture" },
+          { link: "/about/roadmap", text: "Roadmap" },
+          { link: "/about/contributing", text: "Contributing" },
+          { link: "/about/getting-support", text: "Getting Support" },
         ],
-        collapsed: false
+        collapsed: false,
       },
       {
-        text: 'Guides',
+        text: "Guides",
         items: [
-          {link: '/guides/create-a-profile', text: 'Create a Profile'},
-          {link: '/guides/view-the-data', text: 'View the Data'},
-          {link: '/guides/map', text: 'Create a Map'},
-          {link: '/guides/import-networks', text: 'Import Networks'},
-          {link: '/guides/create-a-schema', text: 'Create a Schema'}
+          { link: "/guides/create-a-profile", text: "Create a Profile" },
+          { link: "/guides/view-the-data", text: "View the Data" },
+          { link: "/guides/map", text: "Create a Map" },
+          { link: "/guides/import-networks", text: "Import Networks" },
+          { link: "/guides/create-a-schema", text: "Create a Schema" },
         ],
-        collapsed: false
+        collapsed: false,
       },
       {
-        text: 'Developers',
+        text: "Developers",
         items: [
-          {link: '/developers/environments', text: 'Environments'},
-          {link: '/developers/index-api', text: 'Index API'},
-          {link: '/developers/library-api', text: 'Library API'},
-          {link: '/developers/wp-node', text: 'WordPress Profile Plugin'},
-          {link: '/developers/wp-aggregator', text: 'WordPress Map Plugin'}
+          { link: "/developers/environments", text: "Environments" },
+          { link: "/developers/index-api", text: "Index API" },
+          { link: "/developers/library-api", text: "Library API" },
+          { link: "/developers/wp-node", text: "WordPress Profile Plugin" },
+          { link: "/developers/wp-aggregator", text: "WordPress Map Plugin" },
         ],
-        collapsed: false
+        collapsed: false,
       },
       {
-        text: 'FAQs',
+        text: "FAQs",
         items: [
-          {link: '/faqs/general', text: 'General FAQs'},
-          {link: '/faqs/developer', text: 'Developer FAQs'},
-          {link: '/faqs/schema', text: 'Schema FAQs'}
+          { link: "/faqs/general", text: "General FAQs" },
+          { link: "/faqs/developer", text: "Developer FAQs" },
+          { link: "/faqs/schema", text: "Schema FAQs" },
         ],
-        collapsed: false
-      }
+        collapsed: false,
+      },
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/MurmurationsNetwork' }
-    ]
-  }
-})
+      { icon: "github", link: "https://github.com/MurmurationsNetwork" },
+    ],
+  },
+});
