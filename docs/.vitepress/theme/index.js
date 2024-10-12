d3adecb923a57e8af53743a142c27ee598019bfb
Geoff Turk <geoff@ic3.dev>
2024-10-12 18:20:33 +0200

    fix: redirect root to intro page 2

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,22 +1,6 @@
 import DefaultTheme from 'vitepress/theme'
-import { h } from 'vue'
-import { useRoute, useData } from 'vitepress'
 import './custom.css'
 
 export default {
-  ...DefaultTheme,
-  Layout() {
-    const route = useRoute()
-    const { frontmatter } = useData()
-
-    if (route.path === '/') {
-      return h('div', {}, [
-        h('script', {}, `
-          window.location.href = '/about/introduction';
-        `)
-      ])
-    }
-
-    return h(DefaultTheme.Layout)
-  }
+  ...DefaultTheme
 }
--- docs/index.md
+++ docs/index.md
@@ -1,3 +1,10 @@
 ---
-layout: home
+layout: doc
+title: Redirecting...
 ---
+
+<script>
+window.location.href = '/about/introduction';
+</script>
+
+If you are not redirected automatically, follow this [link to the introduction](/about/introduction).
